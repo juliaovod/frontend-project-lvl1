6f013e25c803d85a45e3fa8a17644c7dbd3c8aca
juliaovod <ovoddev@gmail.com>
2022-01-14 23:32:51 +0300

    Step 7

---
+++ src/games/brain-gcd.js
@@ -0,0 +1,33 @@
+import startGame from '../index.js';
+import { getRandomNumber } from '../utils.js';
+
+const getGreatestCommonDivisor = (x = getRandomNumber(), y = getRandomNumber()) => {
+  let counter = x > y ? x : y;
+
+  while (counter !== 1) {
+    if (x % counter === 0 && y % counter === 0) {
+      return counter;
+    }
+
+    counter -= 1;
+  }
+
+  return counter;
+};
+
+const generateExpression = () => `${getRandomNumber()} ${getRandomNumber()}`;
+
+const gameInstruction = 'Find the greatest common divisor of given numbers.';
+
+const getRightAnswer = (expressionStr) => {
+  const [x, y] = expressionStr.split(' ').map(Number);
+  return String(getGreatestCommonDivisor(x, y));
+};
+
+const startBrainGcd = () => startGame(
+  gameInstruction,
+  generateExpression,
+  getRightAnswer,
+);
+
+export default startBrainGcd;
