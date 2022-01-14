2589df487073621ffffe8bfd947b5f60a1b30cb7
juliaovod <ovoddev@gmail.com>
2022-01-14 23:26:43 +0300

    Add min arg to getRandomNumber util

--- src/utils.js
+++ src/utils.js
@@ -1,2 +1,2 @@
 // eslint-disable-next-line import/prefer-default-export
-export const getRandomNumber = (max = 50) => Math.floor(Math.random() * max);
+export const getRandomNumber = (max = 50, min = 1) => Math.floor(Math.random() * (max - min) + min);
