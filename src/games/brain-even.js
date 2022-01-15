9036768b980d4782988cab1daf6801b55e9506a7
juliaovod <ovoddev@gmail.com>
2022-01-15 14:33:30 +0300

    Cosmetic changes

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,11 +1,11 @@
 import startGame from '../index.js';
 import { getRandomNumber } from '../utils.js';
 
-export const isEven = (number) => Number(number) % 2 === 0;
+export const isEven = (number) => number % 2 === 0;
 
 const gameInstructions = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const getRightAnswer = (number) => (isEven(number) ? 'yes' : 'no');
+const getRightAnswer = (number) => (isEven(Number(number)) ? 'yes' : 'no');
 
 const startBrainEven = () => startGame(
   gameInstructions,
